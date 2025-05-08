5c0ce43e2f9cd28d0ceb331eeda5d37495542494
Thinh Nguyen <37232471+nguyenthinhit996@users.noreply.github.com>
2025-05-08 20:16:28 +0700

    ddd

--- src/pages/Index.tsx
+++ src/pages/Index.tsx
@@ -40,7 +40,7 @@ const Index = () => {
       </div>
       
       {/* Content Sections */}
-      <div className="container mx-auto px-4 py-24 mt-200">
+      <div className="container mx-auto px-4 py-24 mt-[200px]">
         <LoanFeatures />
         
         <div className="my-16">
